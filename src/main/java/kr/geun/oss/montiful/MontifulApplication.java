ecae087216a99daf1d954fe4f3e3e7066d98878e
akageun <akageun@gmail.com>
2019-01-24 18:16:52 +0900

    First Commit

---
+++ src/main/java/kr/geun/oss/montiful/MontifulApplication.java
@@ -0,0 +1,14 @@
+package kr.geun.oss.montiful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontifulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MontifulApplication.class, args);
+	}
+
+}
+
